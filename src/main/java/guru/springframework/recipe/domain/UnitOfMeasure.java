238037f01cccdeba900c0cf3913118b57cf901a8
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-11-13 12:29:49 +0100

    141 One to One Ingredient to UnitOfMeasure

--- src/main/java/guru/springframework/recipe/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/recipe/domain/UnitOfMeasure.java
@@ -1,7 +1,25 @@
 package guru.springframework.recipe.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
 /**
  * @author Krzysztof Kukla
  */
+@Entity
+@Getter
+@Setter
 public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
 }
