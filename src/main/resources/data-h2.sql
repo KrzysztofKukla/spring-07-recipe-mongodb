326c331ad86ea7a95717a529215f8a2b2e7b48f1
Krzysztof Kukla <krzys.kukla@gmail.com>
2019-12-25 22:52:21 +0100

    319 bootstraps for mysql and h2 for different profiles

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,5 +1,9 @@
---This is Spring Boot feature - DML - Data Manipulation Language - need be place in data.sql
---to apply that data.sql file has to be place in classpath - resources
+-- here we added dash '-h2' and that means that it will apply only for H2 database, so only for default profile from
+-- appliaction-profile.yml
+
+
+--This is Spring Boot feature - DML - Data Manipulation Language - need be place in data-h2.sql
+--to apply that data-h2.sql file has to be place in classpath - resources
 -- will be loaded on startup
 INSERT INTO category (description) VALUES ('American');
 INSERT INTO category (description) VALUES ('Italian');
