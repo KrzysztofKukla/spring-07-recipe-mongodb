eac29aa3726c24a31ff6721049da28288634dbd6
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-30 21:34:05 +0200

    356 @ControllerAdrive with @ExceptionHandler

--- src/main/java/guru/springframework/recipe/exception/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/recipe/exception/ControllerExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import org.thymeleaf.exceptions.TemplateInputException;
 
 /**
@@ -18,10 +19,17 @@ public class ControllerExceptionHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
     public String handleException(Exception exception, Model model) {
-        log.error("Handling not found exception");
-        log.error(exception.getMessage());
+        log.error("Handling not found exception {}", exception.getMessage());
         model.addAttribute("exception", exception);
         return "404error";
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleWebExchangeBindException(Exception exception, Model model) {
+        log.error("Binding exception {}", exception.getMessage());
+
+        return "400error";
     }
 
 }
