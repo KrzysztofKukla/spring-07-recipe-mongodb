f2343d38780d6a1805c8b9f8dd5bd62a82067812
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-30 23:18:04 +0200

    358 RouterFunction<T> routes

---
+++ src/main/java/guru/springframework/recipe/config/WebConfig.java
@@ -0,0 +1,31 @@
+package guru.springframework.recipe.config;
+
+import guru.springframework.recipe.domain.Recipe;
+import guru.springframework.recipe.service.RecipeService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+/**
+ * @author Krzysztof Kukla
+ */
+@Configuration
+public class WebConfig {
+
+    @Bean
+    //this is specific for Reactive WebFlux context as a route
+    //here we define ServerResponse with ok status, contentType and body for this particular url request GET '/api/recipes'
+    //for body we are going to call out 'recipeService.findAll()
+    public RouterFunction<?> routes(RecipeService recipeService) {
+        return RouterFunctions.route(RequestPredicates.GET("/api/recipes"),
+            serverRequest -> ServerResponse
+                .ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(recipeService.findAll(), Recipe.class));
+    }
+
+}
